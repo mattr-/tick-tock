5240a5980cd2911b1621a378166ef80a062c2b45
Matt Rogers <mattrogers@sbcglobal.net>
2012-07-24 21:27:49 -0500

    initial rails app skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TickTock::Application.config.secret_token = '8d46a7c7f3c3327c2ed46d134cd118e9c93682d808c771410b325614fa2563067330c9c74b239e93fa404d99ebe60ccaf4d94e688ff6e9893b7076f120a93096'
