ea427f0e1d75ee3ce3fdebbe10363d6891e7fada
Matt Rogers <mattrogers@sbcglobal.net>
2012-07-26 13:47:34 -0500

    Store OAuth details with the user
    Save the OAuth token and the OAuth secret with the user so we can
    use them to load tweets and post tweets as the currently logged in user.

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120725032001) do
+ActiveRecord::Schema.define(:version => 20120726183728) do
 
   create_table "users", :force => true do |t|
     t.string   "provider"
     t.string   "uid"
     t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+    t.string   "oauth_token"
+    t.string   "oauth_secret"
   end
 
 end
