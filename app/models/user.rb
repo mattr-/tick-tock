d73b7602a56db35be5775af21a9239e60c9298c3
Matt Rogers <mattrogers@sbcglobal.net>
2012-07-27 21:04:33 -0500

    store the user's oauth credentials in the database

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :provider, :uid
 
+  def self.from_omniauth(auth)
+    user = where(auth.slice("provider", "uid")).first || create_from_omniauth(auth)
+    user.oauth_token = auth[:credentials][:token]
+    user.oauth_secret = auth[:credentials][:secret]
+    user
+  end
+
   def self.create_from_omniauth(auth)
     create! do |user|
       user.provider = auth['provider']
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,18 +2,35 @@ require 'spec_helper'
 
 describe User do
 
-  it "should create users from omniauth data" do
-    auth = { :provider => 'twitter',
-             :uid => '123434',
-             :info => {
-               :nickname => 'foo'
-             }
+  before :each do
+    @auth = { 
+      :provider => 'twitter',
+      :uid => '123434',
+      :info => {
+        :nickname => 'foo'
+      },
+      :credentials => {
+        :token => 'qwezsdfer',
+        :secret => 'zxzcvafwes'
+      }
     }
+  end
 
-    new_user = User.create_from_omniauth(auth)
-    new_user.provider.should == auth['provider']
-    new_user.uid.should == auth['uid']
-    new_user.name.should == auth[:info]['nickname']
+  it "should create users from omniauth data" do
+    new_user = User.create_from_omniauth(@auth)
+    new_user.provider.should == @auth['provider']
+    new_user.uid.should == @auth['uid']
+    new_user.name.should == @auth[:info]['nickname']
+  end
+
+  it "should find existing users from omniauth data" do
+    User.create_from_omniauth(@auth)
+    new_user = User.from_omniauth(@auth)
+    new_user.provider.should == @auth['provider']
+    new_user.uid.should == @auth['uid']
+    new_user.name.should == @auth[:info]['nickname']
+    new_user.oauth_token.should == @auth[:credentials][:token]
+    new_user.oauth_secret.should == @auth[:credentials][:secret]
   end
 
 end
